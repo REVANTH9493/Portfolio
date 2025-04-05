ec2a8fdb96f3fb130ad035eba6c71bfd7d6ceebe
revanth <61309883+REVANTH9493@users.noreply.github.com>
2025-04-05 18:15:37 +0530

    Update ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
   const form = useRef(null);
